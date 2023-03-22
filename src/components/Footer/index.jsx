777d7b44016d4cb571a3d8fed6893f52d4e7a437
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-22 11:39:30 -0300

    add footer, imgs e links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,128 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import LogoAmazul from '../../assets/images/logos/logoAmazul.png';
+import Logo5s from '../../assets/images/logos/logo5s.png';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div
+      className="footer-bg footer py-3"
+      style={{
+        backgroundColor: 'var(--greenBg)',
+        color: 'var(--grayTextlight)'
+      }}
+      id="links-importantes"
+    >
+      <div className="container">
+        <h4 className="text-center lh-1">Links Importantes</h4>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 border-top">
+          {/* <!-- <div className="col mb-3"></div> --> */}
+
+          <div className="col mb-3 ">
+            <h5>Mapa do Site</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <Link to="/" className="nav-link p-0">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item mb-2">
+                <Link to="/flora" className="nav-link p-0">
+                  Flora
+                </Link>
+              </li>
+              <li className="nav-item mb-2">
+                <Link to="/fauna" className="nav-link p-0">
+                  Fauna
+                </Link>
+              </li>
+              <li className="nav-item mb-2">
+                <Link to="/arqueologia" className="nav-link p-0">
+                  Arqueologia
+                </Link>
+              </li>
+              <li className="nav-item mb-2">
+                <Link to="/aguas" className="nav-link p-0">
+                  Águas
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col mb-3">
+            <h5>Parceiros</h5>
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2">
+                <a
+                  href="https://www.gov.br/cnen/pt-br"
+                  target="_blank"
+                  className="nav-link p-0"
+                >
+                  CNEN
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="https://www.marinha.mil.br/ctmsp/"
+                  target="_blank"
+                  className="nav-link p-0"
+                >
+                  CTMSP
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  target="_blank"
+                  href="https://ipero.sp.gov.br/"
+                  className="nav-link p-0"
+                >
+                  Prefeitura de Iperó
+                </a>
+              </li>
+              <li className="nav-item mb-2">
+                <a
+                  href="https://www.icmbio.gov.br/flonaipanema/"
+                  target="_blank"
+                  className="nav-link p-0"
+                >
+                  ICMBio
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col mb-3"></div>
+          <div className="col mb-3"></div>
+          <div className="col mb-3 d-flex flex-column align-items-center justify-content-between text-center">
+            <div className="d-flex">
+              <a
+                href="https://www.amazul.mar.mil.br/rmb_atuacao_projetos"
+                target="_blank"
+                className="d-flex align-items-center justify-content-center link-dark text-decoration-none"
+              >
+                <img src={LogoAmazul} alt="Logo Amazul" width="60" />
+              </a>
+              <a
+                href={LogoAmazul}
+                target="_blank"
+                className="d-flex align-items-center justify-content-center link-dark text-decoration-none"
+              >
+                <img src={Logo5s} alt="Logo GI" width="60" />
+              </a>
+            </div>
+
+            <p className="py-3">
+              Este repositório é uma iniciativa da Gerência de Implantação da
+              <b> AMAZUL </b>no RMB.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <p className="footer-p text-muted">
+          Copyright © 2022 RLC Arruda - Gerência de Implantação Amazul - Todos
+          os direitos reservados
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,84 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.css';
 
 export default function Header() {
   return (
-    // <div className="menu-container">
-    //   <nav
-    //     className="navbar navbar-expand-lg navbar-dark"
-    //     data-bs-theme="dark"
-    //     style={{
-    //       backgroundColor: 'var(--greenBg)'
-    //     }}
-    //   >
-    //     <div classNameName="container-fluid d-flex justify-content-">
-    //       <Link className="navbar-brand" to="/">
-    //         Ações Ambientais | RMB
-    //       </Link>
-    //       <button
-    //         className="navbar-toggler"
-    //         type="button"
-    //         data-bs-toggle="collapse"
-    //         data-bs-target="#navbarNavDropdown"
-    //         aria-controls="navbarNavDropdown"
-    //         aria-expanded="false"
-    //         aria-label="Toggle navigation"
-    //       >
-    //         <span className="navbar-toggler-icon"></span>
-    //       </button>
-    //       <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    //         <ul className="navbar-nav">
-    //           <li className="nav-item">
-    //             <Link className="nav-link active" aria-current="page"></Link>
-    //           </li>
-    //           <li className="nav-item">
-    //             <Link className="nav-link" to="/flora">
-    //               Flora
-    //             </Link>
-    //           </li>
-
-    //           <li className="nav-item">
-    //             <Link className="nav-link" href="#links-importantes">
-    //               Links Importantes
-    //             </Link>
-    //           </li>
-    //           <li className="nav-item dropdown">
-    //             <Link
-    //               className="nav-link dropdown-toggle"
-    //               role="button"
-    //               data-bs-toggle="dropdown"
-    //               aria-expanded="false"
-    //               id="viveiros"
-    //             >
-    //               Viveiros
-    //             </Link>
-    //             <ul className="dropdown-menu">
-    //               <li>
-    //                 <a
-    //                   className="dropdown-item"
-    //                   href="https://drive.google.com/file/d/1irjRqrJmyrBm6qNMXhOOkfTBrJt-ohsZ/view?usp=sharing"
-    //                   target="_blanck"
-    //                 >
-    //                   Sementeira - Casa P4
-    //                 </a>
-    //               </li>
-    //               <li>
-    //                 <a
-    //                   className="dropdown-item"
-    //                   href="https://drive.google.com/file/d/1EOQozKbjeocNsIi3K3YNf4Z0fnuzQhts/view?usp=sharing"
-    //                   target="_blanck"
-    //                 >
-    //                   Rustificação - Casa P6
-    //                 </a>
-    //               </li>
-    //             </ul>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </nav>
-    // </div>
     <nav
       className="navbar navbar-expand-lg navbar-dark"
       data-bs-theme="dark"
@@ -87,9 +11,10 @@ export default function Header() {
       }}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
+        <Link className="navbar-brand" to="/">
+          Ações Ambientais
+        </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -104,19 +29,29 @@ export default function Header() {
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
+              <Link className="nav-link" to="flora">
+                Flora
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
+              <Link className="nav-link" to="/fauna">
+                Fauna
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/arqueologia">
+                Arqueologia
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/aguas">
+                Águas
+              </Link>
             </li>
             <li className="nav-item dropdown">
               <a
@@ -126,24 +61,30 @@ export default function Header() {
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Dropdown link
+                Viveiros
               </a>
               <ul className="dropdown-menu">
                 <li>
-                  <a className="dropdown-item" href="#">
-                    Action
+                  <a
+                    className="dropdown-item"
+                    href="https://drive.google.com/file/d/1irjRqrJmyrBm6qNMXhOOkfTBrJt-ohsZ/view?usp=sharing"
+                  >
+                    Mapa das Sementeiras
                   </a>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
+                  <a
+                    className="dropdown-item"
+                    href="https://drive.google.com/file/d/1EOQozKbjeocNsIi3K3YNf4Z0fnuzQhts/view?usp=sharing"
+                  >
+                    Mapa da Rustificação
                   </a>
                 </li>
-                <li>
+                {/* <li>
                   <a className="dropdown-item" href="#">
                     Something else here
                   </a>
-                </li>
+                </li> */}
               </ul>
             </li>
           </ul>
