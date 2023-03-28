e79b834978cdecc74010c4d632d425386310071a
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-28 12:39:01 -0300

    fix: add img na mesma linha do titulo da featureItem

--- src/components/FeatureList/FeatureItem.jsx
+++ src/components/FeatureList/FeatureItem.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 export default function FeatureItem(props) {
   return (
     <div className=" py-2">
-      <div className=" col">
+      <div className="col d-flex gap-2">
         <div className="d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 rounded-3">
           <img src={props.icon} />
         </div>
         <h3 className="fs-4">{props.title}</h3>
-        <p>{props.children}</p>
       </div>
+      <p>{props.children}</p>
     </div>
   );
 }
