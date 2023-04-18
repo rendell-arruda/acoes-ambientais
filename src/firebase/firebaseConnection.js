2ac9951b3ae3dbe73b7bbbde4001c5ada0207bec
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-18 08:25:31 -0300

    import storage firebase

--- src/firebase/firebaseConnection.js
+++ src/firebase/firebaseConnection.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCR_aIWndGrK_olWIOISgBX31ImEO0hxgU',
@@ -13,7 +14,9 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
+const storage = getStorage(firebaseApp);
 
 export { db, auth };
