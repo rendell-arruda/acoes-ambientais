218817e2a020337902aa33a14cef2c3292bdd4ad
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-29 20:16:30 -0300

    inicia page profile

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../context/auth';
 import Sidebar from '../../components/Sidebar';
 import TitleRegis from '../../components/Texts/TitleRegis';
 import { FcSettings } from 'react-icons/fc';
+import { FiUpload } from 'react-icons/fi';
+import avatar from '../../assets/images/logos/avatar.png';
 
 export default function Profile() {
+  const { user } = useContext(AuthContext);
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+
   return (
     <div>
       <Sidebar />
@@ -11,6 +17,32 @@ export default function Profile() {
         <TitleRegis name="Minha Conta">
           <FcSettings size={25} />
         </TitleRegis>
+
+        <div className="containerRegis">
+          <form className="form-profile">
+            <label className="label-avatar">
+              <label>
+                <FiUpload color="#FFF" size={25} />
+              </label>
+              <input type="file" accept="image/*" /> <br />
+              {avatarUrl === null ? (
+                <img
+                  src={avatar}
+                  alt="Foto de perfil"
+                  width={250}
+                  height={250}
+                />
+              ) : (
+                <img
+                  src={avatarUrl}
+                  alt="Foto de perfil"
+                  width={250}
+                  height={250}
+                />
+              )}
+            </label>
+          </form>
+        </div>
       </div>
     </div>
   );
